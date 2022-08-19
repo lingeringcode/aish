d90320d6c9839ede3186e767213c37c9dba2782b
Chris Lindgren <chris.a.lindgren@gmail.com>
2022-08-19 15:10:00 -0400

    update journals

---
+++ aish.php
@@ -0,0 +1,51 @@
+<?php
+  /* 
+   Code modified from: https://davidwalsh.name/php-cache-function
+  */
+  // Gets the contents of a file if it exists, otherwise grabs and caches */
+  function get_content($file,$url,$hours = 24) {
+    //vars
+    $current_time = time(); $expire_time = $hours * 60 * 60; $file_time = filemtime($file);
+    //decisions, decisions
+    if(file_exists($file) && ($current_time - $expire_time < $file_time)) {
+      //echo 'returning from cached file';
+      return file_get_contents($file);
+    }
+    else {
+      $content = get_url($url);
+      $content.= '<!-- cached:  '.time().'-->';
+      $file_path = "tmp/".$file;
+      file_put_contents($file_path,$content);
+      return $content;
+    }
+  }
+
+  /* gets content from a URL via curl */
+  function get_url($url) {
+    $ch = curl_init();
+    curl_setopt($ch,CURLOPT_URL,$url);
+    curl_setopt($ch,CURLOPT_RETURNTRANSFER,1); 
+    curl_setopt($ch,CURLOPT_CONNECTTIMEOUT,5);
+    $content = curl_exec($ch);
+    curl_close($ch);
+    return $content;
+  }
+  /* Retrieve feeds */
+  $xml_feed_list = array(
+    "https://journals.sagepub.com/action/showFeed?ui=0&mi=ehikzz&ai=2b4&jc=jbtb&type=etoc&feed=rss",
+    "https://journals.sagepub.com/action/showFeed?ui=0&mi=ehikzz&ai=2b4&jc=jtwa&type=etoc&feed=rss",
+    "https://journals.sagepub.com/action/showFeed?ui=0&mi=ehikzz&ai=2b4&jc=wcxa&type=etoc&feed=rss",
+    "https://www.tandfonline.com/action/showFeed?type=etoc&feed=rss&jc=htcq20",
+    "https://www.tandfonline.com/action/showFeed?type=etoc&feed=rss&jc=hrhr20",
+    "https://www.tandfonline.com/action/showFeed?type=etoc&feed=rss&jc=rrsq20",
+    "https://rss.sciencedirect.com/publication/science/87554615",
+    "https://journals.sagepub.com/action/showFeed?ui=0&mi=ehikzz&ai=2b4&jc=nmsa&type=etoc&feed=rss"
+  );
+
+  $i = 1;
+  foreach( $xml_feed_list as $feed_url ) {
+    $fu = "journal-".( (string) ($i) ).".xml";
+    $FEED_XML = get_content($fu, $feed_url, 3);
+    $i++;
+  }
+?>
